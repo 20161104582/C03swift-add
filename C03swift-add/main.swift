62f4db9dd56cb3b24a17e367d6769844afafe9f8
20161104582 <a20161104582@scy.local>
2018-09-12 11:44:47 +0800

    C03swift-add
    sum+=I

---
+++ C03swift-add/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  C03swift-add
+//
+//  Created by a20161104582 on 2018/9/12.
+//  Copyright © 2018年 a20161104582. All rights reserved.
+//
+
+import Foundation
+
+var sum = 0
+for i in 1...100{
+    sum = sum+i
+}
+print(sum)
+
+
+
